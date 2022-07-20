93431bc1f7c8d0a7bd4ac708aee2506428ac51c6
Maria Coitinho <brasildu@gmail.com>
2022-07-20 12:11:48 -0500

    commit tree 4f8095fe8d936fe72eaff9713715e1df57a53408

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Add your code here
+
+// fetch(destinationURL, configurationObject)
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {"Content-Type": "application/json",
+//               "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: 'cutie',
+//         dogBreed: 'pomeranian'
+//     })
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+// ------------USING CATCH--------------
+
+//     const configurationObject = {
+//         headers: {"Content-Type": "application/json",
+//                   "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             dogName: 'cutie',
+//             dogBreed: 'pomeranian'
+//         })
+//     }    
+
+
+// fetch("http://localhost:3000/dogs",
+// configurationObject)
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => {
+//         alert("Bad things! Ragnarok!")
+//         console.log(error.message)})
+
+
+
+
+// ----------------- LAB -----------------
+
+
+
+function submitData(name, email) {
+   return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {"Content-Type": "application/json",
+              "Accept": "application/json"},
+    body: JSON.stringify({
+          name,
+          email
+    })          
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+submitData()
